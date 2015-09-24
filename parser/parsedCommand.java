b03365c5eb478da1ac14a8bb863025ec42bcb98f
KY <chua.kayi@yahoo.com.sg>
2015-09-24 14:52:04 +0800

    Updated ParsedCommand (NOT READY FOR USE)

--- parser/parsedCommand.java
+++ parser/parsedCommand.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Locale;
 
-public class parsedCommand {
+import MyTextBuddy.COMMAND_TYPE;
+
+public class ParsedCommand {
 	private enum CommandType {
-		ADD, DELETE, EDIT, DISPLAY, ERROR;
+		ADD, DELETE, EDIT, DISPLAY, ERROR, INVALID, EXIT;
 	}
 	
 	private String input;
@@ -18,20 +20,144 @@ public class parsedCommand {
 	private Calendar end;
 	private String description;
 	private ArrayList<String> tags;
-	private String errorMessage;
+	private String messageToUser;
 	private int taskId;
 	
 	private static final String ERROR_NO_INPUT = "Error: No user input";
+	private static final int INDEX_FOR_START = 0;
+	private static final int INDEX_FOR_END = 1;
+	private static final String ERROR_MISSING_ARGS = "Error: No arguments entered";
+	private static final int INDEX_FOR_ARGS = 1;
 	
-	public parsedCommand(String userInput) {
+	public ParsedCommand(CommandType cmdType, String title, Calendar start, Calendar end, 
+			             String description, ArrayList<String> tags, String msg, int taskId) {
+		this.cmdType = cmdType;
+		this.title = title;
+		this.start = start;
+		this.end = end;
+		this.description = description;
+		this.tags = tags;
+		this.messageToUser = msg;
+		this.taskId = taskId;
+	}
+	
+	public ParsedCommand parseCommand(String userInput) {
 		if (userInput.length() == 0) {
-			setError(ERROR_NO_INPUT);
+			return createParsedCommandError(ERROR_NO_INPUT);
+		} else {
+			String input[] = userInput.split(" ", 2);
+			String userCommand = input[INDEX_FOR_COMMAND];
+			CommandType command = determineCommandType(userCommand);
+			
+			switch (command) {
+		        case ADD :
+		    	    return createParsedCommandAdd(input);
+		    	    
+		        case DELETE :
+		        	return createParsedCommandDelete(input);
+		        	
+		        case EDIT :
+		        	return createParsedCommandEdit(input);
+
+		        case DISPLAY :
+		        	return createParsedCommandDisplay(input);
+		        	
+		        case INVALID :
+		        	return createParsedCommandError(ERROR_INVALID_COMMAND);
+		        	
+		        case EXIT : 
+		        	return createParsedCommandExit();
+		        
+		        default :
+		        	// is never visited
+		        	throw new Error("ERROR");
+			} 
+		}
+	}
+	
+	private ParsedCommand createParsedCommandExit() {
+		ParsedCommand pc = new ParsedCommand(CommandType.EXIT, null, null, null, null, null, MESSAGE_EXIT, 0);
+		return pc;
+	}
+
+	private ParsedCommand createParsedCommandDisplay(String input) {
+		ParsedCommand pc = new ParsedCommand(CommandType.DISPLAY, null, null, null, null, null, null, 0);
+		return pc;
+	}
+
+	private ParsedCommand createParsedCommandEdit(String[] input) {
+		String inputArgs = input[INDEX_FOR_ARGS];
+		if (inputArgs == null) {
+			return createParsedCommandError(ERROR_MISSING_ARGS);
+		} else {
+			String title = StringParser.getTitleFromString(inputArgs);
+			Calendar[] times = StringParser.getDatesTimesFromString(inputArgs);
+			Calendar start = times[INDEX_FOR_START];
+			Calendar end = times[INDEX_FOR_END];
+			String description = StringParser.getDescriptionFromString(inputArgs);
+			int taskId = StringParser.getTaskIdFromString(inputArgs);
+			ArrayList<String> tags = StringParser.getTagsFromString(inputArgs);
+			
+			ParsedCommand pc = new ParsedCommand(CommandType.EDIT, title, start, end, description,
+					                             tags, null, taskId);
+			return pc;
+		}
+	}
+
+	private ParsedCommand createParsedCommandDelete(String[] input) {
+		String inputArgs = input[INDEX_FOR_ARGS];
+		if (inputArgs == null) {
+			return createParsedCommandError(ERROR_MISSING_ARGS);
+		} else {
+			int taskId = StringParser.getTaskIdFromString(inputArgs);
+			ParsedCommand pc = new ParsedCommand(CommandType.DELETE, null, null, null, null,
+                    tags, null, taskId);
+			return pc;
+		}
+	}
+
+	private ParsedCommand createParsedCommandAdd(String[] input) {
+		String inputArgs = input[INDEX_FOR_ARGS];
+		if (inputArgs == null) {
+			return createParsedCommandError(ERROR_MISSING_ARGS);
+		} else {
+			String title = StringParser.getTitleFromString(inputArgs);
+			Calendar[] times = StringParser.getDatesTimesFromString(inputArgs);
+			Calendar start = times[INDEX_FOR_START];
+			Calendar end = times[INDEX_FOR_END];
+			String description = StringParser.getDescriptionFromString(inputArgs);
+			ArrayList<String> tags = StringParser.getTagsFromString(inputArgs);
+			
+			ParsedCommand pc = new ParsedCommand(CommandType.ADD, title, start, end, description,
+					                             tags, null, 0);
+			return pc;
+		}
+	}
+
+	private ParsedCommand createParsedCommandError(String errorMsg) {
+		ParsedCommand pc = new ParsedCommand(CommandType.ERROR, null, null, null, null, null, errorMsg, 0);
+		return pc;
+	}
+	
+	private static CommandType determineCommandType(String commandTypeString) {
+		if (commandTypeString.equalsIgnoreCase("add")) {
+			return CommandType.ADD;
+		} else if (commandTypeString.equalsIgnoreCase("delete")) {
+			return CommandType.DELETE;
+		} else if (commandTypeString.equalsIgnoreCase("show")) {
+			return CommandType.DISPLAY;
+		} else if (commandTypeString.equalsIgnoreCase("edit")) {
+			return CommandType.EDIT;
+		} else if (commandTypeString.equalsIgnoreCase("exit")) {
+		 	return CommandType.EXIT;
+		} else {
+			return CommandType.INVALID;
 		}
 	}
 
 	private void setError(String errorMsg) {
 		this.cmdType = CommandType.ERROR;
-		this.errorMessage = errorMsg;
+		this.messageToUser = errorMsg;
 	}
 	
 	public CommandType getCommandType() {
@@ -59,7 +185,7 @@ public class parsedCommand {
 	}
 	
 	public String getErrorMessage() {
-		return this.errorMessage;
+		return this.messageToUser;
 	}
 	
 	public int getTaskId() {
