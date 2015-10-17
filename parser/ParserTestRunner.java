f0e4b1b2ab4afe3ee154a53a1dcc9ea924c2c897
KY <chua.kayi@yahoo.com.sg>
2015-10-18 02:49:39 +0800

    Added test runner for parser tests

---
+++ parser/ParserTestRunner.java
@@ -0,0 +1,16 @@
+package parser;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class ParserTestRunner {
+	public static void main(String[] args) {
+	      Result result = JUnitCore.runClasses(ParserTestSuite.class);
+	      for (Failure failure : result.getFailures()) {
+	         System.out.println(failure.toString());
+	      }
+	      System.out.println(result.wasSuccessful());
+	   }
+}
+
---
+++ parser/ParserTestSuite.java
@@ -0,0 +1,17 @@
+package parser;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+   ParsedCommandTest.class,
+   DateTimeParserTest.class,
+   TimeParserTest.class,
+   FormattedDateTimeParserTest.class,
+   FlexibleDateTimeParserTest.class,
+   NattyDateTimeParserTest.class
+})
+public class ParserTestSuite {
+
+}
