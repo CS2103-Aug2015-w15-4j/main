38aab15fa96268e1d888e9047f52e545d335faa6
KY <chua.kayi@yahoo.com.sg>
2015-10-31 17:14:55 +0800

    Narrowed down acceptable Natty formats

--- parser/NattyDateTimeParser.java
+++ parser/NattyDateTimeParser.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.joestelmach.natty.DateGroup;
 import com.joestelmach.natty.Parser;
@@ -15,10 +17,29 @@ public class NattyDateTimeParser extends DateTimeParser{
 	private String unparsedInput;
 	private String[] nattyTimes;
 	private String[] nattyDates;
+	private static final String DAYS = "(mon(day)?|tue?(sday)?|tues|wed(nesday)?|thur(sday)?|thu|thurs|fri(day)?|sat(urday)?|sun(day)?|tmr|tomorrow|tomorow|today|tdy|ytd|yesterday)s?";
+	private static final String RELATIVE_WORDS = "(next|nxt|prev|previous|last|coming|upcoming)";
+	private static final String DURATION = "(day(s?)|month(s?)|year(s?)|yrs)";
+	private static final String NUMBERS = "(\\d+)(?=\\s" + "(DURATION" + "|" + DAYS + ")" + ")";
+	private static final String TO_ALTERNATIVES = "(?<=\\s|^)(until|til|till|-)(?=\\s|$)";
 	
+	private static final String NATTY_REGEX = "(?<=\\s|^)(" + DAYS + "|" + RELATIVE_WORDS + "|" + DURATION + "|" + NUMBERS + "|to)(?=\\s|$)";
+	private static final Pattern NATTY_DATES = Pattern.compile(NATTY_REGEX);
 	
 	private static final Logger logger = Logger.getLogger(NattyDateTimeParser.class.getName() );
-
+	
+	public static String standardiseNattyInput(String input) {
+		input = input.replaceAll(TO_ALTERNATIVES, "to");
+		input = input.toLowerCase();
+		Matcher m = NATTY_DATES.matcher(input);
+		String stdForm = "";
+		while (m.find()) {
+			stdForm = stdForm + m.group() + " ";
+		}
+		System.out.println(stdForm);
+		return stdForm;
+	}
+	
 	public static Calendar[] parseDateTimeWithNatty(String input) {
 		
 		Calendar[] parsedDates = new Calendar[2];
@@ -64,6 +85,7 @@ public class NattyDateTimeParser extends DateTimeParser{
     	assert(parsedTimes != null);
     	logger.log(Level.FINE, "INPUT TO NATTY: " + input);
     	// Times parsed by now, only dates left
+    	input = standardiseNattyInput(input);
     	if (canParseWithNatty(input)) { // can parse without time, ie has date
     		String[] startAndEndDates = input.split(" to | until | til | till | - ");
     		String dateWithTime;
