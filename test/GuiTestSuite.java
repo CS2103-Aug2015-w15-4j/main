42138d64558c606d1feb48d9e137b772a6f0898e
Vylantze <digi9346@hotmail.com>
2015-11-11 14:55:42 +0800

    Test suite

---
+++ test/GuiTestSuite.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//@author A0122534R
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ 
+	GuiShortcutTest.class,
+	GuiControllerTest.class,
+})
+
+
+public class GuiTestSuite {
+
+}
